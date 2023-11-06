b4275a179007ea07becb24509a56720123230ce4
programmerNizam <mdnizam12109@gmail.com>
2023-11-07 02:14:12 +0600

    Project completed without responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
 const App = () => {
   return (
     <div className="container">
       <Navbar />
+      <Hero />
     </div>
   );
 };
